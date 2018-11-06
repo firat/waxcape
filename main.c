d3c8ebfb6858601b70229c3de2413465cea65f79
Firat Basarir <firatcanbasarir@gmail.com>
2018-11-06 14:06:14 -0800

    Simpler waxcape that exclusively locks a single device.

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <libinput.h>
-#include <libudev.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
@@ -13,81 +12,112 @@
 
 static volatile sig_atomic_t stop = 0;
 static struct libevdev_uinput* waxcape_keyboard;
-static bool exclusive_grab = 0;
-static int count = 0;
+static bool debug = 0;
+
+enum waxcape_state { NOT_PRESSED, PRESSED, CTRL_PRESSED };
+
+static enum waxcape_state caps_state = NOT_PRESSED;
+
+static void print_key_name(int32_t key, int state) {
+	char *key_name;
+	char key_arr [1024];
+	sprintf(key_arr, "%d", key);
+	switch (key) {
+		case KEY_CAPSLOCK:
+			key_name = "Caps Lock";
+			break;
+		case KEY_LEFTCTRL:
+			key_name = "Left Ctrl";
+			break;
+		case KEY_ESC:
+			key_name = "Escape";
+			break;
+		default:
+			key_name = key_arr;
+	}
+
+
+	printf("Key: %s, state: %d\n", key_name, state);
+}
+
 
 static void waxcape_handle_key_event(struct libinput_event* ev) {
     enum libinput_event_type ev_type = libinput_event_get_type(ev);
-    struct libinput_device* dev = libinput_event_get_device(ev);
+    if (ev_type == LIBINPUT_EVENT_KEYBOARD_KEY) {
+        struct libinput_event_keyboard* k =
+            libinput_event_get_keyboard_event(ev);
+        enum libinput_key_state state =
+            libinput_event_keyboard_get_key_state(k);
+        uint32_t key = libinput_event_keyboard_get_key(k);
 
-    struct libinput_event_keyboard* k = libinput_event_get_keyboard_event(ev);
-    enum libinput_key_state state = libinput_event_keyboard_get_key_state(k);
-    uint32_t key = libinput_event_keyboard_get_key(k);
+		if (debug) {
+			print_key_name(key, state);
+		}
 
-    printf("Got event type: %d from %s - Key: %c, state: %d\n", ev_type,
-           libinput_device_get_name(dev), key, state);
+        switch (caps_state) {
+        case NOT_PRESSED:
+            if (key == KEY_CAPSLOCK) {
+                caps_state = PRESSED;
+            } else {
+                // Just report the key event, since capslock is not pressed.
+                libevdev_uinput_write_event(waxcape_keyboard, EV_KEY, key,
+                                            state);
+                libevdev_uinput_write_event(waxcape_keyboard, EV_SYN,
+                                            SYN_REPORT, 0);
+            }
+            break;
+        case PRESSED:
+            if (key == KEY_CAPSLOCK) {
+                if (!state) {
+                    // capslock is released without another key, report escape
+                    libevdev_uinput_write_event(waxcape_keyboard, EV_KEY,
+                                                KEY_ESC, 1);
+                    libevdev_uinput_write_event(waxcape_keyboard, EV_KEY,
+                                                KEY_ESC, 0);
+                    libevdev_uinput_write_event(waxcape_keyboard, EV_SYN,
+                                                SYN_REPORT, 0);
+                    caps_state = NOT_PRESSED;
+                }
+            } else {
+                // If another key is pressed when CAPS_LOCK is pressed, we send
+                // CAPS as ctrl
+                libevdev_uinput_write_event(waxcape_keyboard, EV_KEY,
+                                            KEY_LEFTCTRL, 1);
+                libevdev_uinput_write_event(waxcape_keyboard, EV_KEY, key,
+                                            state);
+                libevdev_uinput_write_event(waxcape_keyboard, EV_SYN,
+                                            SYN_REPORT, 0);
+                caps_state = CTRL_PRESSED;
+            }
+            break;
+        case CTRL_PRESSED:
+            if (key == KEY_CAPSLOCK) {
+                if (!state) {
+                    // Release the ctrl
+                    libevdev_uinput_write_event(waxcape_keyboard, EV_KEY,
+                                                KEY_LEFTCTRL, 0);
+                    libevdev_uinput_write_event(waxcape_keyboard, EV_SYN,
+                                                SYN_REPORT, 0);
+                    caps_state = NOT_PRESSED;
+                }
+            } else {
+				// Report key event without changing state since CTRL is not released.
+				libevdev_uinput_write_event(waxcape_keyboard, EV_KEY, key,
+                                            state);
+                libevdev_uinput_write_event(waxcape_keyboard, EV_SYN,
+                                            SYN_REPORT, 0);
 
-    libevdev_uinput_write_event(waxcape_keyboard, EV_KEY, key, state);
-    libevdev_uinput_write_event(waxcape_keyboard, EV_SYN, SYN_REPORT, 0);
-}
-
-static bool is_waxcape_device(const char* device_name) {
-    // return strcmp(uinput_name, device_name) == 0;
-	return true;
+			}
+            break;
+        }
+    }
 }
 
 static void waxcape_handle_events(struct libinput* li) {
     struct libinput_event* ev;
     libinput_dispatch(li);
     while ((ev = libinput_get_event(li)) != NULL) {
-        struct libinput_device* dev = libinput_event_get_device(ev);
-		printf("sysname: %s\n", libinput_device_get_sysname(dev));
-		printf("name: %s\n", libinput_device_get_name(dev));
-
-		
-		printf("uinput syspath: %s\n", libevdev_uinput_get_syspath(waxcape_keyboard));
-		printf("uinput devnode: %s\n", libevdev_uinput_get_devnode(waxcape_keyboard));
-
-        struct udev_device* udev_dev = libinput_device_get_udev_device(dev);
-		const char *dev_syspath = udev_device_get_syspath(udev_dev);
-		struct udev_list_entry *udev_links = udev_device_get_devlinks_list_entry(udev_dev);
-		do {
-			printf("udev devlink: %s\n", udev_list_entry_get_value(udev_links));
-		} while ((udev_links = udev_list_entry_get_next(udev_links)) != NULL);
-		printf("udev devpath: %s\n", udev_device_get_devpath(udev_dev));
-		printf("udev subsystem: %s\n", udev_device_get_subsystem(udev_dev));
-		printf("udev devtype: %s\n", udev_device_get_devtype(udev_dev));
-		printf("udev syspath: %s\n", udev_device_get_syspath(udev_dev));
-		printf("udev sysname: %s\n", udev_device_get_sysname(udev_dev));
-		printf("udev sysnum: %s\n", udev_device_get_sysnum(udev_dev));
-		printf("udev devnode: %s\n", udev_device_get_syspath(udev_dev));
-        enum libinput_event_type ev_type = libinput_event_get_type(ev);
-        if (ev_type == LIBINPUT_EVENT_DEVICE_ADDED) {
-            if (is_waxcape_device(dev_syspath) ||
-                !libinput_device_has_capability(dev,
-                                                LIBINPUT_DEVICE_CAP_KEYBOARD)) {
-
-                printf("Ignoring device %s\n", libinput_device_get_name(dev));
-            } else {
-
-                /*
-
-                                if (grab && *grab && ioctl(fd, EVIOCGRAB,
-                   (void*)1) == -1) { fprintf(stderr, "Grab requested, but
-                   failed for %s(%s)\n", path, strerror(errno));
-                                }
-
-                                struct libinput_device* dev =
-                   libinput_event_get_device(ev); printf("Registered new device:
-                               %s.\n", libinput_device_get_name(dev));
-                                           */
-            }
-        }
-
-        if (ev_type == LIBINPUT_EVENT_KEYBOARD_KEY) {
-            waxcape_handle_key_event(ev);
-        }
-
+        waxcape_handle_key_event(ev);
         libinput_event_destroy(ev);
         libinput_dispatch(li);
     }
@@ -100,7 +130,7 @@ static void waxcape_poll_and_handle_events_loop(struct libinput* li) {
     fds.events = POLLIN;
     fds.revents = 0;
 
-    while (count++ < 10 && !stop && poll(&fds, 1, -1) > -1) {
+    while (!stop && poll(&fds, 1, -1) > -1) {
 
         waxcape_handle_events(li);
     }
@@ -119,31 +149,15 @@ static int waxcape_register_signal_handler() {
     return sigaction(SIGINT, &act, NULL);
 }
 
-static bool waxcape_create_keyboard() {
-    int err;
-    struct libevdev* dev = libevdev_new();
-    libevdev_set_name(dev, "Waxcape Keyboard");
-
-    libevdev_enable_event_type(dev, EV_KEY);
-    printf("Creating the new keyboard\n");
-    libevdev_enable_event_code(dev, EV_KEY, KEY_A, NULL);
-
-    err = libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED,
-                                             &waxcape_keyboard);
-
-    if (err != 0) {
-        printf("Failed: %s\n", strerror(errno));
-        return false;
-    }
-
-    return true;
-}
-
 static int open_restricted(const char* path, int flags, void* user_data) {
     int fd = open(path, flags);
 
     if (fd < 0)
         fprintf(stderr, "Failed to open %s (%s)\n", path, strerror(errno));
+    if (ioctl(fd, EVIOCGRAB, (void*)1) == -1) {
+        fprintf(stderr, "Grab requested, but failed for %s (%s)\n", path,
+                strerror(errno));
+    }
 
     return fd < 0 ? -errno : fd;
 }
@@ -155,38 +169,71 @@ const static struct libinput_interface interface = {
     .close_restricted = close_restricted,
 };
 
-static struct libinput* waxcape_create_libinput_context(struct udev* udev) {
-    struct libinput* li;
-    const char* seat = "seat0";
-    li = libinput_udev_create_context(&interface, NULL, udev);
-    if (!li) {
-        fprintf(stderr, "Failed to create libinput context: %s\n",
-                strerror(errno));
-        return NULL;
-    }
-
-    libinput_udev_assign_seat(li, seat);
-    return li;
-}
+void print_usage() { printf("USAGE: waxcape <device>\n"); }
 
 int main(int argc, char** argv) {
+    if (argc != 2) {
+        print_usage();
+        return 1;
+    }
+
     if (waxcape_register_signal_handler() == -1) {
         fprintf(stderr, "Failed to set up signal handling (%s)\n",
                 strerror(errno));
-        return -errno;
+        return 1;
     }
 
-    if (!waxcape_create_keyboard()) {
-        return -errno;
+    const char* src_keyboard = argv[1];
+    int src_fd = open(src_keyboard, O_RDONLY);
+    int rc = 0;
+    if (src_fd == -1) {
+        fprintf(stderr, "Failed to open device %s: %s\n", src_keyboard,
+                strerror(errno));
+        return 1;
     }
-    struct udev* udev = udev_new();
-    struct libinput* li = waxcape_create_libinput_context(udev);
+    struct libevdev* evdev;
+    rc = libevdev_new_from_fd(src_fd, &evdev);
+    if (rc < 0) {
+        fprintf(stderr, "Failed to create evdev device: %d %s\n", -rc,
+                strerror(-rc));
+        return 1;
+    }
+
+    struct libevdev_uinput* foo;
+    rc = libevdev_uinput_create_from_device(evdev, LIBEVDEV_UINPUT_OPEN_MANAGED,
+                                            &foo);
+    if (rc < 0) {
+        fprintf(stderr, "Failed to create waxcape keyboard: %d %s\n", -rc,
+                strerror(-rc));
+        return 1;
+    }
+
+    waxcape_keyboard = foo;
+
+    // We don't need the evdev and fd once we create the uinput instance.
+    libevdev_free(evdev);
+    close(src_fd);
+
+    struct libinput* li;
+    li = libinput_path_create_context(&interface, NULL);
+    if (!li) {
+        fprintf(stderr, "Failed to create libinput context: %s\n",
+                strerror(errno));
+        return 1;
+    }
+
+	// Sleep 1 second before capturing the keyboard so the enter key doesn't stay stuck.
+    sleep(1);
+    if (!libinput_path_add_device(li, src_keyboard)) {
+        fprintf(stderr, "Failed to capture input: %s\n", src_keyboard);
+        return 1;
+    }
 
     waxcape_poll_and_handle_events_loop(li);
 
-    libevdev_uinput_destroy(waxcape_keyboard);
+    // Clean up
     libinput_unref(li);
-    udev_unref(udev);
+    libevdev_uinput_destroy(waxcape_keyboard);
     printf("Done.\n");
 
     return 0;
